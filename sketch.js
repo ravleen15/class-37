0cbacb260e9e2f77749421fbf1fb7c8d940aa198
ravleen15 <75840960+ravleen15@users.noreply.github.com>
2021-01-25 19:40:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+  var allPlayers;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+
+var car1,car2,car3,car4,cars
+
+
+function setup(){
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+
+  background("white")
+
+  if(playerCount==4){
+    game.update(1)
+  }
+  if(gameState==1){
+    game.play()
+  }
+}
